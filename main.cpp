b8fb348935b4b0cddd38a5e49857a5a6c9b1de6b
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2020-03-04 12:34:56 +0100

    Created Repository
    One small step for a man, but a giant step for the bois.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "SDL/include/SDL.h"
+#include "SDL_Image/include/SDL_image.h"
+#include "SDL_Mixer/include/SDL_mixer.h"
+
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+#pragma comment(lib,"SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib,"SDL_Mixer/libx86/SDL2_mixer.lib")
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
